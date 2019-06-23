07bad74bda35557f095fbcfc0d6cb139a7ecf512
diony <dionyuci@gmail.com>
2019-06-23 13:30:53 +0200

    sdsd

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
+window.addEventListener('load', function() {
+  var isPushEnabled = false;
+
+  var pushButton = document.querySelector('.js-push-button');
+  pushButton.addEventListener('click', function() {
+    if (isPushEnabled) {
+      unsubscribe();
+    } else {
+      subscribe();
+    }
+  });
+
+
 if ('serviceWorker' in navigator) {
-  /*navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))*/
-
-
-  navigator.serviceWorker.register('sw.js').then(
-      function(serviceWorkerRegistration) {
-        serviceWorkerRegistration.pushManager.subscribe().then(
-            function(pushSubscription) {
-              console.log(pushSubscription.subscriptionId);
-              console.log(pushSubscription.endpoint);
-              // The push subscription details needed by the application
-              // server are now available, and can be sent to it using,
-              // for example, an XMLHttpRequest.
-            }, function(error) {
-              // During development it often helps to log errors to the
-              // console. In a production environment it might make sense to
-              // also report information about errors back to the
-              // application server.
-              console.log(error);
-            }
-        );
-      });
-
+  navigator.serviceWorker.register('sw.js')
+    .then(initialiseState)
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
 
 }
+
+
+  // Once the service worker is registered set the initial state
+  function initialiseState() {
+
+    console.log("Entro en inicializar");
+
+    // Are Notifications supported in the service worker?
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+      console.warn('Notifications aren\'t supported.');
+      return;
+    }
+
+    // Check the current Notification permission.
+    // If its denied, it's a permanent block until the
+    // user changes the permission
+    if (Notification.permission === 'denied') {
+      console.warn('The user has blocked notifications.');
+      return;
+    }
+
+    // Check if push messaging is supported
+    if (!('PushManager' in window)) {
+      console.warn('Push messaging isn\'t supported.');
+      return;
+    }
+
+    // We need the service worker registration to check for a subscription
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+      // Do we already have a push message subscription?
+      serviceWorkerRegistration.pushManager.getSubscription()
+          .then(function(subscription) {
+            // Enable any UI which subscribes / unsubscribes from
+            // push messages.
+            var pushButton = document.querySelector('.js-push-button');
+            pushButton.disabled = false;
+
+            if (!subscription) {
+              // We aren't subscribed to push, so set UI
+              // to allow the user to enable push
+              return;
+            }
+
+            // Keep your server in sync with the latest subscriptionId
+            //sendSubscriptionToServer(subscription);
+
+            // Set your UI to show they have subscribed for
+            // push messages
+            pushButton.textContent = 'Disable Push Messages';
+            isPushEnabled = true;
+          })
+          .catch(function(err) {
+            console.warn('Error during getSubscription()', err);
+          });
+    });
+  }
 
 
